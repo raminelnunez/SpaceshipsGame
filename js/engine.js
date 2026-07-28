f142e4be61e1f6cd5a883170c4a4831924c72ea5
Raminel <Raminelnc23@gmail.com>
2026-07-28 17:49:57 -0500

    tie character animations to runtime speed

--- js/engine.js
+++ js/engine.js
@@ -1,19 +1,17 @@
-var Engine = (function(global) {
-
+var Engine = (function (global) {
   var doc = global.document,
-      win = global.window,
+    win = global.window,
     canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     lastTime = 0,
     accumulator = 0,
     step = 1 / 60;
 
-      canvas.width = 1100;
-      canvas.height = 800;
+  canvas.width = 1100;
+  canvas.height = 800;
 
   function main() {
-     
-      var now = Date.now(),
+    var now = Date.now(),
       frameTime = Math.min(0.25, (now - lastTime) / 1000.0);
 
     accumulator += frameTime;
@@ -23,102 +21,103 @@ var Engine = (function(global) {
       accumulator -= step;
     }
 
-      render();
+    render();
 
-      lastTime = now;
+    lastTime = now;
 
-      win.requestAnimationFrame(main);
+    win.requestAnimationFrame(main);
   }
 
-
   function init() {
-      reset();
-      lastTime = Date.now();
-      main();
+    reset();
+    lastTime = Date.now();
+    main();
   }
 
   function update(dt) {
-      updateEntities(dt);
+    updateEntities(dt);
   }
 
   function updateEntities(dt) {
-      allEnemies.forEach(function(enemy) {
-          enemy.update(dt);
-      });
-      player.update();
+    allEnemies.forEach(function (enemy) {
+      enemy.update(dt);
+    });
+    player.update();
+    updateAnimations();
+  }
+
+  function updateAnimations() {
+    player.tickCount++;
+
+    if (player.tickCount > player.ticksPerFrame) {
+      player.tickCount = 0;
+      player.frameIndex++;
+    }
+
+    if (player.frameIndex >= player.numberOfFrames) {
+      player.frameIndex = 0;
+    }
+
+    allEnemies.forEach(function (enemy) {
+      enemy.tickCount++;
+
+      if (enemy.tickCount > enemy.ticksPerFrame) {
+        enemy.tickCount = 0;
+        enemy.frameIndex++;
+      }
+
+      if (enemy.frameIndex >= enemy.numberOfFrames) {
+        enemy.frameIndex = 0;
+      }
+    });
   }
 
   function render() {
-     
-      ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      ctx.drawImage(Resources.get('images/background.png'), 0, 0);
+    ctx.drawImage(Resources.get("images/background.png"), 0, 0);
 
-      renderEntities();
+    renderEntities();
   }
 
   function renderEntities() {
+    ctx.drawImage(Resources.get("images/sun.png"), sun.x, sun.y);
 
-      ctx.drawImage(Resources.get('images/sun.png'), sun.x, sun.y);
+    const playerImage = Resources.get("images/player.png");
 
+    ctx.drawImage(
+      playerImage,
+      0,
+      (player.frameIndex * playerImage.naturalHeight) / player.numberOfFrames,
+      playerImage.naturalWidth,
+      playerImage.naturalHeight / player.numberOfFrames,
+      player.x,
+      player.y,
+      playerImage.naturalWidth,
+      playerImage.naturalHeight / player.numberOfFrames,
+    );
 
-      player.tickCount++;
-
-      if (player.tickCount > player.ticksPerFrame) {
-        player.tickCount = 0;
-        player.frameIndex++;
-      }
-
-      if(player.frameIndex >= player.numberOfFrames) {
-        player.frameIndex = 0;
-      }
-
-      const playerImage = Resources.get('images/player.png');
-
-      ctx.drawImage(
-        playerImage,
-        0,
-        player.frameIndex * playerImage.naturalHeight / player.numberOfFrames,
-        playerImage.naturalWidth,
-        playerImage.naturalHeight / player.numberOfFrames,
-        player.x,
-        player.y,
-        playerImage.naturalWidth,
-        (playerImage.naturalHeight) / player.numberOfFrames
+    allEnemies.forEach(function (enemy) {
+      const image = Resources.get(
+        `images/${enemy.style}-${enemy.direction}.png`,
       );
 
-      allEnemies.forEach(function(enemy) {
-        enemy.tickCount++;
-
-        if (enemy.tickCount > enemy.ticksPerFrame) {
-          enemy.tickCount = 0;
-          enemy.frameIndex++;
-        }
-
-        if(enemy.frameIndex >= enemy.numberOfFrames) {
-          enemy.frameIndex = 0;
-        }
-
-        const image = Resources.get(`images/${enemy.style}-${enemy.direction}.png`);
-
-        ctx.drawImage(
-          image,
-          0,
-          enemy.frameIndex * image.naturalHeight / enemy.numberOfFrames,
-          image.naturalWidth,
-          image.naturalHeight / enemy.numberOfFrames,
-          enemy.x,
-          enemy.y,
-          image.naturalWidth,
-          (image.naturalHeight) / enemy.numberOfFrames
-        );
-
-      });
-
+      ctx.drawImage(
+        image,
+        0,
+        (enemy.frameIndex * image.naturalHeight) / enemy.numberOfFrames,
+        image.naturalWidth,
+        image.naturalHeight / enemy.numberOfFrames,
+        enemy.x,
+        enemy.y,
+        image.naturalWidth,
+        image.naturalHeight / enemy.numberOfFrames,
+      );
+    });
   }
 
   function reset() {
-    allEnemies.forEach(function(enemy) {
+    allEnemies.forEach(function (enemy) {
       enemy.tickCount = 0;
       enemy.ticksPerFrame = 5;
       enemy.frameIndex = 0;
@@ -132,17 +131,17 @@ var Engine = (function(global) {
   }
 
   Resources.load([
-      'images/background.png',
-      'images/enemy1-right.png',
-      'images/enemy2-right.png',
-      'images/enemy3-right.png',
-      'images/enemy1-left.png',
-      'images/enemy2-left.png',
-      'images/enemy3-left.png',
-      'images/enemy4-left.png',
-      'images/enemy4-right.png',
-      'images/sun.png',
-      'images/player.png',
+    "images/background.png",
+    "images/enemy1-right.png",
+    "images/enemy2-right.png",
+    "images/enemy3-right.png",
+    "images/enemy1-left.png",
+    "images/enemy2-left.png",
+    "images/enemy3-left.png",
+    "images/enemy4-left.png",
+    "images/enemy4-right.png",
+    "images/sun.png",
+    "images/player.png",
   ]);
 
   Resources.onReady(init);
