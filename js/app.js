8bbec3c30370c2c8b0ca28b4794981cd6991f32c
Raminel <Raminelnc23@gmail.com>
2026-07-28 17:43:22 -0500

    refactor key event handling for improved readability and organization

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 const screenLimit = {
   width: [0, 1000],
   height: [0, 700],
-}
+};
 function getRandomNum(max) {
-  return Math.floor(Math.random() * (max +1))
+  return Math.floor(Math.random() * (max + 1));
 }
-
 
 function chance(percentage) {
-  return getRandomNum(100) < percentage; 
+  return getRandomNum(100) < percentage;
 }
 
 function difference(a, b) {
@@ -16,18 +15,18 @@ function difference(a, b) {
 }
 
 function between(a, b) {
-  return (a+b)/2;
+  return (a + b) / 2;
 }
 
 function isCloseTo(a, b, margin) {
   if (difference(a.x, b.x) <= margin) {
     if (difference(a.y, b.y) <= margin) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
   } else {
-    return false
+    return false;
   }
 }
 
@@ -38,12 +37,12 @@ class Sun {
   }
 }
 class Enemy {
-  constructor ([x,y], style, speed) {
+  constructor([x, y], style, speed) {
     this.x = x;
     this.y = y;
     this.style = style;
     this.speed = speed;
-    this.direction = 'right';
+    this.direction = "right";
     this.move = this.chasePlayer;
     this.id = getRandomNum(9999);
   }
@@ -55,7 +54,7 @@ class Enemy {
     if (this.x < screenLimit.width[0]) {
       this.x = screenLimit.width[0] + 1;
     }
-  
+
     if (this.y > screenLimit.height[1]) {
       this.y = screenLimit.height[1] - 1;
     }
@@ -65,7 +64,7 @@ class Enemy {
     if (isCloseTo(this, sun, 100)) {
       this.goAway(sun.x, sun.y, 1);
     }
-    for(let enemy of allEnemies) {
+    for (let enemy of allEnemies) {
       if (enemy.id !== this.id) {
         if (isCloseTo(this, enemy, 50)) {
           this.goAway(enemy.x, enemy.y, 1);
@@ -83,21 +82,21 @@ class Enemy {
   }
 
   runBack() {
-    const center = screenLimit.width[1]/2;
+    const center = screenLimit.width[1] / 2;
     let targetX = between(center, player.x);
 
-    const back = screenLimit.height[1] /3;
+    const back = screenLimit.height[1] / 3;
     if (player.y < back) {
-      this.goTo(targetX, player.y, 1.2); 
+      this.goTo(targetX, player.y, 1.2);
     } else if (player.y > back) {
       this.goTo(targetX, back, 1.2);
     }
   }
 
   flank() {
-    const center = screenLimit.width[1]/2;
-    const rightFlank = center*1.67;
-    const leftFlank = center*0.33;
+    const center = screenLimit.width[1] / 2;
+    const rightFlank = center * 1.67;
+    const leftFlank = center * 0.33;
     if (this.x >= center && this.x < rightFlank) {
       this.goTo(rightFlank, this.y, 2);
     } else if (this.x < center && this.x > leftFlank) {
@@ -106,73 +105,83 @@ class Enemy {
 
     if (this.x >= rightFlank || this.x <= leftFlank) {
       this.goTo(this.x, player.y, 2);
-      if (difference(this.y, player.y) < (screenLimit.height[1]/5) *4) {
+      if (difference(this.y, player.y) < (screenLimit.height[1] / 5) * 4) {
         this.move = this.chasePlayer;
       }
     }
   }
 
   displace(dir, speed) {
-    if (dir === 'up') { this.y = this.y - speed}
-    if (dir === 'down') { this.y = this.y + speed}
-    if (dir === 'left') { this.x = this.x - speed; this.direction = 'left'}
-    if (dir === 'right') { this.x = this.x + speed; this.direction = 'right'}
+    if (dir === "up") {
+      this.y = this.y - speed;
+    }
+    if (dir === "down") {
+      this.y = this.y + speed;
+    }
+    if (dir === "left") {
+      this.x = this.x - speed;
+      this.direction = "left";
+    }
+    if (dir === "right") {
+      this.x = this.x + speed;
+      this.direction = "right";
+    }
   }
 
   goTo(x, y, speedMultiplier) {
     if (this.x < x) {
-      this.displace('right', this.speed*speedMultiplier)
+      this.displace("right", this.speed * speedMultiplier);
     } else if (this.x > x) {
-      this.displace('left', this.speed*speedMultiplier)
+      this.displace("left", this.speed * speedMultiplier);
     }
     if (this.y < y) {
-      this.displace('down', this.speed*speedMultiplier)
+      this.displace("down", this.speed * speedMultiplier);
     } else if (this.y > y) {
-      this.displace('up', this.speed*speedMultiplier)
+      this.displace("up", this.speed * speedMultiplier);
     }
   }
 
   goAway(x, y, speedMultiplier) {
     if (this.x > x) {
-      this.displace('right', this.speed*speedMultiplier)
+      this.displace("right", this.speed * speedMultiplier);
     } else if (this.x < x) {
-      this.displace('left', this.speed*speedMultiplier)
+      this.displace("left", this.speed * speedMultiplier);
     }
     if (this.y > y) {
-      this.displace('down', this.speed*speedMultiplier)
+      this.displace("down", this.speed * speedMultiplier);
     } else if (this.y < y) {
-      this.displace('up', this.speed*speedMultiplier)
+      this.displace("up", this.speed * speedMultiplier);
     }
   }
 
   changeMove() {
     let moves = [this.chasePlayer, this.jockey, this.runBack, this.flank];
     for (let enemy of allEnemies) {
-      moves = moves.filter(move => move !== enemy.move)
+      moves = moves.filter((move) => move !== enemy.move);
     }
     if (moves.length > 0) {
-      this.move = moves[getRandomNum(moves.length-1)];
+      this.move = moves[getRandomNum(moves.length - 1)];
     }
   }
 
   isMoveTaken(move) {
     let isMoveTaken = false;
-    for(let enemy of allEnemies) {
+    for (let enemy of allEnemies) {
       if (enemy.move === move) {
         isMoveTaken = true;
       }
     }
-    return isMoveTaken
+    return isMoveTaken;
   }
 
   isAnyCloseToPlayer() {
     let is = false;
-    for(let enemy of allEnemies) {
+    for (let enemy of allEnemies) {
       if (isCloseTo(enemy, player, 150) && enemy.id !== this.id) {
         is = true;
       }
     }
-    return is
+    return is;
   }
 
   pickMove() {
@@ -182,12 +191,16 @@ class Enemy {
       } else {
         this.move = this.chasePlayer;
       }
-    } 
+    }
     if (isCloseTo(this, player, 200)) {
       if (this.isAnyCloseToPlayer()) {
         if (chance(50)) {
           this.move = this.runBack;
-        } else if (player.y > this.y && difference(player.y, this.y) > difference(this.x, screenLimit.width[1]/2)) {
+        } else if (
+          player.y > this.y &&
+          difference(player.y, this.y) >
+            difference(this.x, screenLimit.width[1] / 2)
+        ) {
           this.move = this.flank;
         }
       } else if (!this.isMoveTaken(this.chasePlayer)) {
@@ -199,7 +212,6 @@ class Enemy {
   }
 
   brain() {
-
     if (chance(1)) {
       this.pickMove();
     }
@@ -207,9 +219,9 @@ class Enemy {
     this.checkPos();
   }
 
-  update = function(dt) {
+  update = function (dt) {
     if (!isGamePaused) {
-      this.brain()
+      this.brain();
     }
   };
 }
@@ -232,7 +244,7 @@ class Player {
       this.x = screenLimit.width[0] + 1;
       return false;
     }
-  
+
     if (this.y > screenLimit.height[1]) {
       this.y = screenLimit.height[1] - 1;
       return false;
@@ -246,78 +258,83 @@ class Player {
 
   update() {
     if (!isGamePaused) {
-      checkLoss()
-      checkWin()
+      checkLoss();
+      checkWin();
       this.displace();
     }
   }
 
   letGo(key) {
-    this.directions = this.directions.filter(dir => dir !== key)
+    this.directions = this.directions.filter((dir) => dir !== key);
   }
 
   pressed(key) {
     const opposites = {
-      'up': 'down',
-      'down': 'up',
-      'left': 'right',
-      'right': 'left'
-    }
+      up: "down",
+      down: "up",
+      left: "right",
+      right: "left",
+    };
 
     if (key === undefined) {
       this.directions = [];
     } else {
-      this.directions = this.directions.filter(dir => dir !== opposites[key])
+      this.directions = this.directions.filter((dir) => dir !== opposites[key]);
       if (!this.directions.includes(key)) {
         this.directions.push(key);
       }
     }
   }
 
-  handleBoost() {
-
-  }
+  handleBoost() {}
 
   displace() {
     if (this.checkPos()) {
-      if (this.directions.includes('up')) { this.y = this.y - this.speed}
-      if (this.directions.includes('down')) { this.y = this.y + this.speed}
-      if (this.directions.includes('left')) { this.x = this.x - this.speed}
-      if (this.directions.includes('right')) { this.x = this.x + this.speed}
+      if (this.directions.includes("up")) {
+        this.y = this.y - this.speed;
+      }
+      if (this.directions.includes("down")) {
+        this.y = this.y + this.speed;
+      }
+      if (this.directions.includes("left")) {
+        this.x = this.x - this.speed;
+      }
+      if (this.directions.includes("right")) {
+        this.x = this.x + this.speed;
+      }
     }
   }
-  
 }
 
 const html = {
-  level: document.getElementById('level'),
-  lives: document.getElementById('lives'),
-  canvas: document.getElementById('canvas'),
-  score: document.getElementById('score'),
-  seconds: document.getElementById('seconds'),
-  message_to_player: document.getElementById('message_to_player'),
-  music_button: document.getElementById('music-button'),
-  pause: document.getElementById('pause')
-}
+  level: document.getElementById("level"),
+  lives: document.getElementById("lives"),
+  canvas: document.getElementById("canvas"),
+  score: document.getElementById("score"),
+  seconds: document.getElementById("seconds"),
+  message_to_player: document.getElementById("message_to_player"),
+  music_button: document.getElementById("music-button"),
+  pause: document.getElementById("pause"),
+};
 
 const audio = {
-  gameOver: new Audio('./audio/game_over.mp3'),
-  win: new Audio('./audio/win.mp3'),
-  youDied: new Audio('./audio/you_died.mp3'),
-  music: new Audio('./audio/background_music.mp3')
-}
+  gameOver: new Audio("./audio/game_over.mp3"),
+  win: new Audio("./audio/win.mp3"),
+  youDied: new Audio("./audio/you_died.mp3"),
+  music: new Audio("./audio/background_music.mp3"),
+};
 
 function clearMessage() {
   html.message_to_player.innerText = "";
-  html.message_to_player.style.visibility = 'hidden';
+  html.message_to_player.style.visibility = "hidden";
   clearTimeout(timeout);
 }
 
 let timeout;
 function displayMessage(message, seconds) {
   html.message_to_player.innerHTML = `<h1>${message}</h1>`;
-  html.message_to_player.style.visibility = 'visible';
-  timeout = setTimeout(clearMessage, (seconds*1000));
+  html.message_to_player.style.visibility = "visible";
+  timeout = setTimeout(clearMessage, seconds * 1000);
 }
 
 let timer;
@@ -329,31 +346,36 @@ let score = 0;
 const PlayerProps = {
   speed: 7,
   lives: 3,
-  startingPos: [screenLimit.width[1]/2, (screenLimit.height[1]/5) *4],
-}
+  startingPos: [screenLimit.width[1] / 2, (screenLimit.height[1] / 5) * 4],
+};
 
-let sun = new Sun(screenLimit.width[1]/2, 10);
-let player = new Player(PlayerProps.startingPos, PlayerProps.lives, PlayerProps.speed);
+let sun = new Sun(screenLimit.width[1] / 2, 10);
+let player = new Player(
+  PlayerProps.startingPos,
+  PlayerProps.lives,
+  PlayerProps.speed,
+);
 let allEnemies = [];
 
 function createEnemyProps(howMany) {
-
   let enemyProps = [];
   for (let i = 0; i < howMany; i++) {
     enemyProps.push({
-      coords: [getRandomNum(screenLimit.width[1]), getRandomNum(screenLimit.height[1]/5)],
-      image: `enemy${i+1}`,
+      coords: [
+        getRandomNum(screenLimit.width[1]),
+        getRandomNum(screenLimit.height[1] / 5),
+      ],
+      image: `enemy${i + 1}`,
       speed: 2.4,
-    })
+    });
   }
 
   return enemyProps;
 }
 
 function initEnemies(props) {
-  
   for (let enemy of props) {
-    allEnemies.push(new Enemy(enemy.coords, enemy.image, enemy.speed))
+    allEnemies.push(new Enemy(enemy.coords, enemy.image, enemy.speed));
   }
 }
 
@@ -373,12 +395,12 @@ function incrementSeconds() {
 }
 
 function addScore() {
-  score += ((level+1) * 10) - seconds;
+  score += (level + 1) * 10 - seconds;
   html.score.innerText = `Score: ${score}`;
 }
 
 function removeScore() {
-  score -= (50 / (level + 1)) + seconds;
+  score -= 50 / (level + 1) + seconds;
   score = Math.round(score);
   if (score < 0) {
     score = 0;
@@ -390,25 +412,25 @@ function newLevel() {
   seconds = 0;
   html.seconds.innerText = `Time: ${seconds}`;
   audio.win.play();
-  level++
-  resetLocations()
+  level++;
+  resetLocations();
   for (let enemy of allEnemies) {
-    enemy.speed = (enemy.speed * (1 + ((level + 1)/50)));
+    enemy.speed = enemy.speed * (1 + (level + 1) / 50);
   }
-  html.level.innerText = `Level: ${level}`
+  html.level.innerText = `Level: ${level}`;
   addScore();
 }
 
 function loss() {
   if (player.lives <= 0) {
     audio.gameOver.play();
-    displayMessage('GAME OVER', 0.5)
-    resetGame()
+    displayMessage("GAME OVER", 0.5);
+    resetGame();
   } else {
     audio.youDied.play();
     resetLocations();
     player.lives += -1;
-    html.lives.innerText = `Lives: ${player.lives}`
+    html.lives.innerText = `Lives: ${player.lives}`;
   }
   removeScore();
 }
@@ -430,7 +452,7 @@ function checkLoss() {
 function resetLocations() {
   for (let i = 0; i < allEnemies.length; i++) {
     allEnemies[i].x = (screenLimit.width[1] / allEnemies.length) * i;
-    allEnemies[i].y = getRandomNum(screenLimit.height[1] / 6)
+    allEnemies[i].y = getRandomNum(screenLimit.height[1] / 6);
   }
   player.x = PlayerProps.startingPos[0];
   player.y = PlayerProps.startingPos[1];
@@ -446,25 +468,29 @@ function resetGame() {
   player.y = PlayerProps.startingPos[1];
   resetEnemies(createEnemyProps(3));
 
-  html.level.innerText = `Level: ${level}`
-  html.lives.innerText = `Lives: ${player.lives}`
-  return
+  html.level.innerText = `Level: ${level}`;
+  html.lives.innerText = `Lives: ${player.lives}`;
+  return;
 }
 
 function initGame() {
   timer = setInterval(incrementSeconds, 1000);
   level = 0;
-  sun = new Sun(screenLimit.width[1]/2, 10);
-  player = new Player(PlayerProps.startingPos, PlayerProps.lives, PlayerProps.speed);
+  sun = new Sun(screenLimit.width[1] / 2, 10);
+  player = new Player(
+    PlayerProps.startingPos,
+    PlayerProps.lives,
+    PlayerProps.speed,
+  );
   initEnemies(createEnemyProps(3));
 
-  html.level.innerText = `Level: ${level}`
-  html.lives.innerText = `Lives: ${player.lives}`
+  html.level.innerText = `Level: ${level}`;
+  html.lives.innerText = `Lives: ${player.lives}`;
   html.score.innerText = `Score: ${score}`;
   html.seconds.innerText = `Time: ${seconds}`;
-  displayMessage('Reach The Sun', 2);
+  displayMessage("Reach The Sun", 2);
   setTimeout(handlePause, 2200);
-  return
+  return;
 }
 
 initGame();
@@ -482,7 +508,7 @@ function handlePause() {
 
 function handleMusic() {
   if (isMusic) {
-    isMusic = false
+    isMusic = false;
     audio.music.pause();
     html.music_button.innerText = "Turn Music ON";
   } else {
@@ -492,29 +518,25 @@ function handleMusic() {
   }
 }
 
-document.addEventListener('keydown', function(e) {
-  const allowedKeys = {
-      37: 'left',
-      38: 'up',
-      39: 'right',
-      40: 'down'
-  };
+const allowedKeys = {
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down",
+  65: "left",
+  87: "up",
+  68: "right",
+  83: "down",
+};
 
+document.addEventListener("keydown", function (e) {
   player.pressed(allowedKeys[e.keyCode]);
 });
 
-document.addEventListener('keyup', function(e) {
-  const allowedKeys = {
-      37: 'left',
-      38: 'up',
-      39: 'right',
-      40: 'down'
-  };
-
+document.addEventListener("keyup", function (e) {
   player.letGo(allowedKeys[e.keyCode]);
 });
 
-html.music_button.addEventListener('click', handleMusic);
+html.music_button.addEventListener("click", handleMusic);
 
-html.pause.addEventListener('click', handlePause);
-
+html.pause.addEventListener("click", handlePause);
